3602272bf23793b8a43210f020a9da41d70ac714
greatxrider <daligdigjeph09@gmail.com>
2023-09-14 19:03:01 +0800

    project finished

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -10,7 +10,7 @@ def sentiment_analyzer(text_to_analyse):
    )
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    response = requests.post(url, json = myobj, headers=header)
+    response = requests.post(url, json = myobj, headers=header, timeout=10)
     formatted_response = json.loads(response.text)
     if response.status_code == 200:
         label = formatted_response['documentSentiment']['label']
